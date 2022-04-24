a46de1082823652756d815dbdba4a1a04de2bf79
HiddenCirno <85435395+HiddenCirno@users.noreply.github.com>
2022-04-24 14:49:09 +0800

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,10 +1,10 @@
-﻿using System;
 using xiaoye97;
 using HarmonyLib;
 using BepInEx;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
+using System;
 using System.Reflection;
 using CommonAPI;
 using CommonAPI.Systems;
@@ -68,12 +68,12 @@ namespace ProjectGenesis
                 proto.IsLabTech = techjson.IsLabTech;
                 proto.PreTechs = techjson.PreTechs;
                 proto.PreTechsImplicit = techjson.PreTechs;
-                proto.Items = techjson.Items ?? Array.Empty<int>();
-                proto.ItemPoints = techjson.ItemPoints ?? Array.Empty<int>();
+                proto.Items = techjson.Items ?? new int[] { };
+                proto.ItemPoints = techjson.ItemPoints ?? new int[] { };
                 proto.HashNeeded = techjson.HashNeeded;
                 proto.UnlockRecipes = techjson.UnlockRecipes;
-                proto.AddItems = techjson.AddItems ?? Array.Empty<int>();
-                proto.AddItemCounts = techjson.AddItemCounts ?? Array.Empty<int>();
+                proto.AddItems = techjson.AddItems ?? new int[] { };
+                proto.AddItemCounts = techjson.AddItemCounts ?? new int[] { };
                 proto.Position = new Vector2(techjson.Position[0], techjson.Position[1]);
 
                 if (!LDB.techs.Exist(techjson.ID))
@@ -98,7 +98,7 @@ namespace ProjectGenesis
                     proto.FuelType = itemjson.FuelType;
                     proto.HeatValue = itemjson.HeatValue;
                     proto.ReactorInc = itemjson.ReactorInc;
-                    proto.DescFields = itemjson.DescFields ?? Array.Empty<int>();
+                    proto.DescFields = itemjson.DescFields ?? new int[] { };
                     proto.IsFluid = itemjson.IsFluid;
                     proto.MiningFrom = itemjson.MiningFrom;
                     proto.PreTechOverride = itemjson.PreTechOverride;
@@ -118,14 +118,14 @@ namespace ProjectGenesis
                     proto.FuelType = itemjson.FuelType;
                     proto.HeatValue = itemjson.HeatValue;
                     proto.ReactorInc = itemjson.ReactorInc;
-                    proto.DescFields = itemjson.DescFields ?? Array.Empty<int>();
+                    proto.DescFields = itemjson.DescFields ?? new int[] { };
                     proto.IsFluid = itemjson.IsFluid;
                     proto.Type = (EItemType)itemjson.Type;
                     proto.SubID = itemjson.SubID;
                     proto.MiningFrom = itemjson.MiningFrom;
                     proto.ProduceFrom = itemjson.ProduceFrom;
                     proto.Grade = itemjson.Grade;
-                    proto.Upgrades = itemjson.Upgrades ?? Array.Empty<int>();
+                    proto.Upgrades = itemjson.Upgrades ?? new int[] { };
                     proto.IsEntity = itemjson.IsEntity;
                     proto.CanBuild = itemjson.CanBuild;
                     proto.BuildInGas = itemjson.BuildInGas;
@@ -152,8 +152,8 @@ namespace ProjectGenesis
                         recipeJson.GridIndex = TableID * 1000 + (recipeJson.GridIndex - 3000);
 
                     var proto = ProtoRegistry.RegisterRecipe(recipeJson.ID, (ERecipeType_1)recipeJson.Type, recipeJson.Time,
-                        recipeJson.Input, recipeJson.InCounts, recipeJson.Output ?? Array.Empty<int>(),
-                        recipeJson.OutCounts ?? Array.Empty<int>(), recipeJson.Description, recipeJson.PreTech, recipeJson.GridIndex, recipeJson.Name, recipeJson.IconPath);
+                        recipeJson.Input, recipeJson.InCounts, recipeJson.Output ?? new int[] { },
+                        recipeJson.OutCounts ?? new int[] { }, recipeJson.Description, recipeJson.PreTech, recipeJson.GridIndex, recipeJson.Name, recipeJson.IconPath);
 
                     proto.Explicit = recipeJson.Explicit;
                     proto.Name = recipeJson.Name;
@@ -172,8 +172,8 @@ namespace ProjectGenesis
                     proto.TimeSpend = recipeJson.Time;
                     proto.Items =  recipeJson.Input;
                     proto.ItemCounts = recipeJson.InCounts;
-                    proto.Results = recipeJson.Output ?? Array.Empty<int>();
-                    proto.ResultCounts =  recipeJson.OutCounts ?? Array.Empty<int>();
+                    proto.Results = recipeJson.Output ?? new int[] { };
+                    proto.ResultCounts =  recipeJson.OutCounts ?? new int[] { };
                     proto.Description = recipeJson.Description;
                     proto.preTech = LDB.techs.Select(recipeJson.PreTech);
                     proto.GridIndex = recipeJson.GridIndex;
@@ -186,8 +186,48 @@ namespace ProjectGenesis
             }
         }
 
+        private static ModelProto CopyModelProto(int oriId, int id, Color color)
+        {
+            var oriModel = LDB.models.Select(oriId);
+            var model = oriModel.Copy();
+            model.name = id.ToString();
+            model.Name = id.ToString();//这俩至少有一个必须加，否则LDBTool报冲突导致后面null
+            model.ID = id;
+            PrefabDesc desc = oriModel.prefabDesc;
+            model.prefabDesc = new PrefabDesc(id, desc.prefab, desc.colliderPrefab);
+            for (int i = 0; i < model.prefabDesc.lodMaterials.Length; i++)
+            {
+                if (model.prefabDesc.lodMaterials[i] == null) continue;
+                for (int j = 0; j < model.prefabDesc.lodMaterials[i].Length; j++)
+                {
+                    if (model.prefabDesc.lodMaterials[i][j] == null) continue;
+                    model.prefabDesc.lodMaterials[i][j] = new Material(desc.lodMaterials[i][j]);
+                }
+            }
+            try
+            {
+                model.prefabDesc.lodMaterials[0][0].color = color;
+                model.prefabDesc.lodMaterials[1][0].color = color;
+                model.prefabDesc.lodMaterials[2][0].color = color;
+            }
+            catch (Exception)
+            {
+            }
+            model.prefabDesc.hasBuildCollider = true;
+            model.prefabDesc.colliders = desc.colliders;
+            model.prefabDesc.buildCollider = desc.buildCollider;
+            model.prefabDesc.buildColliders = desc.buildColliders;
+            model.prefabDesc.colliderPrefab = desc.colliderPrefab;
+            model.sid = "";
+            model.SID = "";
+            return model;
+        }
         public void PreFix()
         {
+            var TankModel = CopyModelProto(121, 301, Color.blue);
+            TankModel.prefabDesc.fluidStorageCount = 10000000;
+            LDBTool.PreAddProto(TankModel);
+
             LDB.strings.Select(2314).name = "剧毒液体海洋";
             LDB.strings.Select(2314).Name = "剧毒液体海洋";
 
@@ -199,8 +239,15 @@ namespace ProjectGenesis
             LDB.milestones.Select(9).defaultDesc = "你采集了钨矿,宇宙珍奇之一.它是一种用途广泛的新材料.";
             LDB.milestones.Select(9).DefaultDesc = "你采集了钨矿,宇宙珍奇之一.它是一种用途广泛的新材料.";
 
+            LDB.items.Select(物品.聚变发电机).name = "核子能源发电站";
+            LDB.items.Select(物品.聚变发电机).Name = "核子能源发电站";
+
             LDB.items.Select(物品.单极磁石).ID = 6980;
             LDB.items.Select(物品.硫酸).ID = 6998;
+
+           // LDB.techs.Select(1607).preTechArray = new TechProto[] { 科技类.电磁学 };
+            //LDB.techs.Select(1607).PreTechs = new int[] { 1001 };
+            //LDB.techs.Select(1607).PreTechsImplicit = new int[] { 1001 };
 
             LDB.items.OnAfterDeserialize();
         }
@@ -226,6 +273,9 @@ namespace ProjectGenesis
             itemProtos.Select(物品.原油采集站).prefabDesc.minerPeriod = 300000;
             itemProtos.Select(物品.水泵).prefabDesc.minerPeriod = 360000;
 
+            //itemProtos.Select(物品.研究站).prefabDesc.labAssembleSpeed = 2;
+            //itemProtos.Select(物品.研究站).prefabDesc.labResearchSpeed = 2;
+
             itemProtos.Select(物品.电磁轨道弹射器).prefabDesc.ejectorChargeFrame = 20;
             itemProtos.Select(物品.电磁轨道弹射器).prefabDesc.ejectorColdFrame = 10;
 
@@ -240,15 +290,14 @@ namespace ProjectGenesis
             itemProtos.Select(物品.风力涡轮机).prefabDesc.powerCoverRadius = 14.9f;
 
             itemProtos.Select(物品.火力发电机).prefabDesc.genEnergyPerTick = 200000;
-            itemProtos.Select(物品.聚变发电机).prefabDesc.genEnergyPerTick = 2500000;
-            
             itemProtos.Select(物品.太阳能板).prefabDesc.genEnergyPerTick = 40000;
             itemProtos.Select(物品.地热发电机).prefabDesc.genEnergyPerTick = 400000;
+            itemProtos.Select(物品.聚变发电机).prefabDesc.genEnergyPerTick = 2500000;
+            itemProtos.Select(物品.聚变发电机).prefabDesc.useFuelPerTick = 2500000;
             itemProtos.Select(物品.风力涡轮机).prefabDesc.genEnergyPerTick = 50000;
             itemProtos.Select(物品.人造恒星).prefabDesc.genEnergyPerTick = 120000000;
 
             itemProtos.Select(物品.火力发电机).prefabDesc.useFuelPerTick = 200000;
-            itemProtos.Select(物品.聚变发电机).prefabDesc.useFuelPerTick = 2500000;
 
             itemProtos.Select(物品.低速传送带).prefabDesc.beltSpeed = 3;
             itemProtos.Select(物品.高速传送带).prefabDesc.beltSpeed = 5;
@@ -298,7 +347,9 @@ namespace ProjectGenesis
             ItemProto.InitItemIndices();
             ItemProto.InitMechaMaterials();
 
-            foreach (var proto in LDB.items.dataArray)
+            var dataArray = LDB.items.dataArray;
+
+            foreach (var proto in dataArray)
             {
                 StorageComponent.itemIsFuel[proto.ID] = proto.HeatValue > 0L;
                 StorageComponent.itemStackCount[proto.ID] = proto.StackSize;
@@ -317,6 +368,16 @@ namespace ProjectGenesis
             rectTransform.sizeDelta = new Vector2(800f, 500f);
             rectTransform.anchoredPosition
                 = new Vector2(rectTransform.anchoredPosition.x, rectTransform.anchoredPosition.y + 50f);
+
+            LDB.models.Select(301).prefabDesc.modelIndex = 301;
+            LDB.items.Select(6229).ModelIndex = 301;
+            LDB.items.Select(6229).prefabDesc = LDB.models.Select(301).prefabDesc;
+            LDB.items.Select(6229).BuildIndex = 501;
+            LDB.items.Select(6229).BuildMode = 1;
+            LDB.items.Select(6229).CanBuild = true;
+            LDB.items.Select(6229).ModelCount = 1;
+            LDB.items.Select(6229).IsEntity = true;
+
         }
     }
 }
