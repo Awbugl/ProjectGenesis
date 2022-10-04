43ea1b28e80512a3862263f0017954855dc14359
Awbugl <littlenine12@163.com>
2022-10-04 16:38:35 +0800

    Update IconDescUtils.cs

--- src/Utils/IconDescUtils.cs
+++ src/Utils/IconDescUtils.cs
@@ -14,7 +14,6 @@ namespace ProjectGenesis.Utils
                                                                            { 7015, new Color32(116, 152, 99, 255) },
                                                                            { 6531, new Color32(187, 217, 219, 255) },
                                                                            { 7014, new Color32(99, 179, 148, 255) },
-                                                                           { 7218, new Color32(167, 91, 0, 255) },
                                                                            { 7013, new Color32(226, 72, 86, 255) },
                                                                            { 7012, new Color32(214, 39, 98, 255) },
                                                                            { 7011, new Color32(51, 255, 173, 255) },
@@ -29,7 +28,7 @@ namespace ProjectGenesis.Utils
                                                                            { 7004, new Color32(115, 177, 74, 255) },
                                                                            { 7003, new Color32(119, 176, 123, 255) },
                                                                            { 7002, new Color32(216, 216, 216, 255) },
-                                                                           { 6999, new Color32(236, 220, 219, 255) },
+                                                                           { 6999, new Color32(255, 243, 128, 255) },
                                                                            { 1116, new Color32(125, 27, 126, 255) },
                                                                            { 7001, new Color32(185, 185, 185, 255) },
                                                                            { 7018, new Color32(90, 126, 179, 255) },
@@ -45,6 +44,7 @@ namespace ProjectGenesis.Utils
                                                                            { 6212, new Color32(222, 214, 0, 255) },
                                                                            { 6210, new Color32(138, 172, 164, 255) },
                                                                            { 6219, new Color32(193, 130, 58, 255) },
+                                                                           { 1114, new Color32(139, 169, 19, 255) },
                                                                            { 6201, new Color32(241, 181, 37, 255) },
                                                                            { 6209, new Color32(230, 81, 21, 255) },
                                                                            { 6208, new Color32(220, 122, 29, 255) },
@@ -53,7 +53,7 @@ namespace ProjectGenesis.Utils
                                                                            { 6235, new Color32(244, 164, 96, 255) }
                                                                        };
 
-        private static readonly Color DefaultSideColor = new Color32(119,136,153, 255),
+        private static readonly Color DefaultSideColor = new Color32(119, 136, 153, 255),
                                       FluidSideColor = new Color32(229, 228, 226, 255),
                                       Color6278 = new Color(1f, 0.4117f, 0.3137f, 0.1961f),
                                       Emission6278 = new Color(1f, 0.2706f, 0f, 0f),
