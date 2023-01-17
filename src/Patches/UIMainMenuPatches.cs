7009257f119247b53324474fbccc88c08752aee2
Awbugl <littlenine12@163.com>
2023-01-18 02:10:54 +0800

    Update UIMainMenuPatches.cs

--- src/Patches/UIMainMenuPatches.cs
+++ src/Patches/UIMainMenuPatches.cs
@@ -5,10 +5,14 @@ using ProjectGenesis.Utils;
 
 public static class UIMainMenuPatches
 {
+    private static bool _shown;
+
     [HarmonyPatch(typeof(VFPreload), "InvokeOnLoadWorkEnded")]
     [HarmonyPostfix]
     public static void OnMainMenuOpen()
     {
+        if (_shown) return;
+
         var sb = new StringBuilder();
 
         if (IncompatibleCheckPatch.GalacticScaleInstalled) sb.AppendLine("GalacticScaleInstalled".TranslateFromJson());
@@ -18,5 +22,7 @@ public static class UIMainMenuPatches
         sb.AppendLine("GenesisBookLoadMessage".TranslateFromJson());
 
         UIMessageBox.Show("GenesisBookLoadTitle".TranslateFromJson(), sb.ToString(), "Ok".TranslateFromJson(), UIMessageBox.INFO);
+
+        _shown = true;
     }
 }
