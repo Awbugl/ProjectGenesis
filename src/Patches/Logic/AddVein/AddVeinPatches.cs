65d70298984e78cb1be15af98ebb59abdf643e40
Awbugl <me@awbugl.top>
2023-11-24 18:49:36 +0800

    Update Code

--- src/Patches/Logic/AddVein/AddVeinPatches.cs
+++ src/Patches/Logic/AddVein/AddVeinPatches.cs
@@ -16,7 +16,7 @@ namespace ProjectGenesis.Patches.Logic.AddVein
         internal static void ModifyVeinData()
         {
             AddVeinProtos(NewVein(15, "铝矿脉", "I铝矿", "Assets/texpack/铝矿脉", 6202, 25),
-                          NewVein(16, "放射性矿脉", "I放射性矿物", "Assets/texpack/放射晶体矿脉", 6222, 31, 4, 90),
+                          NewVein(16, "放射性矿脉", "I放射性矿物", "Assets/texpack/放射晶体矿脉_新", 6222, 31, 4, 90),
                           NewVein(17, "钨矿脉", "I钨矿", "Assets/texpack/钨矿脉", 6201, 34, 1, 120),
                           NewVein(18, "硫矿脉", "I硫矿", "Assets/texpack/硫矿脉", 6207, 34, 4, 90));
 
