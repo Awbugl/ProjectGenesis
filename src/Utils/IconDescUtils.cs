76caf7add242f36fd98abae2ba288a0f05a6f1f6
Awbugl <me@awbugl.top>
2023-12-19 03:33:03 +0800

    Update IconDescUtils.cs

--- src/Utils/IconDescUtils.cs
+++ src/Utils/IconDescUtils.cs
@@ -1,6 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using CommonAPI.Systems;
 using UnityEngine;
+
+// ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
 
 namespace ProjectGenesis.Utils
 {
@@ -8,24 +12,24 @@ namespace ProjectGenesis.Utils
     {
         internal static readonly Dictionary<int, Color32> FluidColor = new Dictionary<int, Color32>
                                                                        {
-                                                                           { 7019, new Color32(129, 199, 241, 255) },//O2
-                                                                           { 6220, new Color32(137, 242, 178, 255) },//N2
-                                                                           { 6234, new Color32(244, 255, 183, 255) },//He4
-                                                                           { 6235, new Color32(210, 222, 142, 255) },//He3
+                                                                           { 7019, new Color32(129, 199, 241, 255) }, //O2
+                                                                           { 6220, new Color32(137, 242, 178, 255) }, //N2
+                                                                           { 6234, new Color32(244, 255, 183, 255) }, //He4
+                                                                           { 6235, new Color32(210, 222, 142, 255) }, //He3
                                                                        };
 
         internal static readonly Dictionary<int, Color32> FluidWithoutIconColor = new Dictionary<int, Color32>
                                                                                   {
-                                                                                      { 1114, new Color32(138, 83, 43, 255) },//Coal Oil
-                                                                                      { 7018, new Color32(97, 132, 186, 255) },//Ocean
-                                                                                      { 7006, new Color32(218, 56, 70, 255) },//苯
-                                                                                      { 7009, new Color32(167, 255, 39, 255) },//丙烯
-                                                                                      { 6206, new Color32(191, 227, 255, 255) },//CO2
-                                                                                      { 6212, new Color32(188, 182, 5, 255) },//JP10
-                                                                                      { 7014, new Color32(104, 187, 154, 255) },//HCl
-                                                                                      { 7015, new Color32(42, 97, 32, 255) },//FeCl3
-                                                                                      { 7002, new Color32(195, 198, 234, 255) },//NH3
-                                                                                      { 7017, new Color32(157, 56, 157, 255) },//HNO3
+                                                                                      { 1114, new Color32(138, 83, 43, 255) },   //Coal Oil
+                                                                                      { 7018, new Color32(97, 132, 186, 255) },  //Ocean
+                                                                                      { 7006, new Color32(218, 56, 70, 255) },   //苯
+                                                                                      { 7009, new Color32(167, 255, 39, 255) },  //丙烯
+                                                                                      { 6206, new Color32(191, 227, 255, 255) }, //CO2
+                                                                                      { 6212, new Color32(188, 182, 5, 255) },   //JP10
+                                                                                      { 7014, new Color32(104, 187, 154, 255) }, //HCl
+                                                                                      { 7015, new Color32(42, 97, 32, 255) },    //FeCl3
+                                                                                      { 7002, new Color32(195, 198, 234, 255) }, //NH3
+                                                                                      { 7017, new Color32(157, 56, 157, 255) },  //HNO3
                                                                                   };
 
         private static readonly Color DefaultSideColor = new Color32(119, 136, 153, 255),
@@ -89,5 +93,71 @@ namespace ProjectGenesis.Utils
 
         private static IconToolNew.IconDesc GetDefaultIconDesc(Color color, Color emission)
             => ProtoRegistry.GetDefaultIconDesc(color, color, emission, emission);
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct MyColor
+    {
+        [FieldOffset(0)]
+        private int rgba;
+
+        [FieldOffset(0)]
+        public byte r;
+
+        [FieldOffset(1)]
+        public byte g;
+
+        [FieldOffset(2)]
+        public byte b;
+
+        [FieldOffset(3)]
+        public byte a;
+
+        public MyColor(
+            byte r,
+            byte g,
+            byte b,
+            byte a)
+        {
+            rgba = 0;
+            this.r = r;
+            this.g = g;
+            this.b = b;
+            this.a = a;
+        }
+
+        public MyColor(
+            float r,
+            float g,
+            float b,
+            float a) : this((byte)(Mathf.Clamp01(r) * (double)byte.MaxValue), (byte)(Mathf.Clamp01(g) * (double)byte.MaxValue),
+                            (byte)(Mathf.Clamp01(b) * (double)byte.MaxValue), (byte)(Mathf.Clamp01(a) * (double)byte.MaxValue)) { }
+
+        public MyColor(string hex)
+        {
+            r = 0;
+            g = 0;
+            b = 0;
+            a = 0;
+
+            switch (hex.Length)
+            {
+                case 8:
+                    rgba = int.Parse(hex, System.Globalization.NumberStyles.HexNumber);
+                    break;
+
+                case 6:
+                    rgba = (int.Parse(hex, System.Globalization.NumberStyles.HexNumber) << 2) + 0xFF;
+                    break;
+
+                default:
+                    throw new ArgumentException("Argument " + nameof(hex) + " : Invavid Length");
+            }
+        }
+
+        public static implicit operator Color(MyColor c)
+            => new Color(c.r / (float)byte.MaxValue, c.g / (float)byte.MaxValue, c.b / (float)byte.MaxValue, c.a / (float)byte.MaxValue);
+
+        public static implicit operator Color32(MyColor c) => new Color32(c.r, c.b, c.g, c.a);
     }
 }
