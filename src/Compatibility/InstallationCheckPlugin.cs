c724093370477b21a1b8f199d5db6687b767538b
Awbugl <me@awbugl.top>
2023-12-30 02:39:35 +0800

    Update InstallationCheckPlugin.cs

--- src/Compatibility/InstallationCheckPlugin.cs
+++ src/Compatibility/InstallationCheckPlugin.cs
@@ -16,7 +16,7 @@ namespace ProjectGenesis.Compatibility
     /// <summary>
     ///     special thanks for https://github.com/kremnev8/DSP-Mods/blob/master/Mods/BlueprintTweaks/InstallationChecker.cs
     /// </summary>
-    [BepInPlugin(MODGUID, MODNAME, VERSION)]
+    [BepInPlugin(MODGUID, MODNAME, ProjectGenesis.VERSION)]
     [BepInDependency(DSPBattle.GUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency(BlueprintTweaks.GUID, BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency(Bottleneck.GUID, BepInDependency.DependencyFlags.SoftDependency)]
@@ -29,7 +29,6 @@ namespace ProjectGenesis.Compatibility
     {
         public const string MODGUID = "org.LoShin.GenesisBook.InstallationCheck";
         public const string MODNAME = "GenesisBook.InstallationCheck";
-        public const string VERSION = "1.0.0";
 
         private static bool _shown;
 
@@ -73,7 +72,7 @@ namespace ProjectGenesis.Compatibility
             if (_shown) return;
             _shown = true;
 
-            string msg = string.Empty;
+            string msg = null;
 
             if (!ProjectGenesis.DisableMessageBoxEntry.Value) msg = "GenesisBookLoadMessage";
 
@@ -83,9 +82,10 @@ namespace ProjectGenesis.Compatibility
 
             if (!PreloaderInstalled) msg = "PreloaderNotInstalled";
 
-            if (!string.IsNullOrEmpty(msg))
-                UIMessageBox.Show("GenesisBookLoadTitle".TranslateFromJson(), msg.TranslateFromJson(), "确定".TranslateFromJson(),
-                                  "跳转交流群".TranslateFromJson(), "跳转日志".TranslateFromJson(), UIMessageBox.INFO, null, OpenBrowser, OpenLog);
+            if (string.IsNullOrEmpty(msg)) return;
+
+            UIMessageBox.Show("GenesisBookLoadTitle".TranslateFromJson(), msg.TranslateFromJson(), "确定".TranslateFromJson(),
+                              "跳转交流群".TranslateFromJson(), "跳转日志".TranslateFromJson(), UIMessageBox.INFO, null, OpenBrowser, OpenLog);
         }
 
         public static void OpenBrowser() => Application.OpenURL("创世之书链接".TranslateFromJson());
