f8d7b9bbc7f9a07771dff4654b816cfefbe3d098
Awbugl <me@awbugl.top>
2025-11-01 22:30:50 +0800

    update assets name

--- src/Patches/AddVein/AddVeinPatches.cs
+++ src/Patches/AddVein/AddVeinPatches.cs
@@ -43,9 +43,9 @@ namespace ProjectGenesis.Patches
         internal static void ModifyVeinData()
         {
             AddVeinProtos(NewVein(15, "铝矿脉", "I铝矿", "Assets/texpack/铝矿脉", ProtoID.I铝矿, 25, 1, 60),
-                NewVein(16, "放射性矿脉", "I放射性矿物", "Assets/texpack/放射晶体矿脉_新新", ProtoID.I放射性矿物, 35, 2, 90),
+                NewVein(16, "放射性矿脉", "I放射性矿物", "Assets/texpack/放射晶体矿脉", ProtoID.I放射性矿物, 35, 2, 90),
                 NewVein(17, "钨矿脉", "I钨矿", "Assets/texpack/钨矿脉", ProtoID.I钨矿, 34, 1, 120),
-                NewVein(18, "硫矿脉", "I硫矿", "Assets/texpack/硫矿脉_新", ProtoID.I硫矿, 36, 1, 90));
+                NewVein(18, "硫矿脉", "I硫矿", "Assets/texpack/硫矿脉", ProtoID.I硫矿, 36, 1, 90));
             return;
 
             VeinProto NewVein(int id, string name, string description, string iconPath, int miningItem, int miningEffect, int modelIndex,
