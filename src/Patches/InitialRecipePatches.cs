6d4b1eeb071e012d33c14a25d2c48bb8c24c8ae3
Awbugl <me@awbugl.top>
2026-02-02 00:09:36 +0800

    Update InitialRecipePatches.cs

--- src/Patches/InitialRecipePatches.cs
+++ src/Patches/InitialRecipePatches.cs
@@ -1,4 +1,6 @@
-﻿using HarmonyLib;
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
 
 // ReSharper disable InconsistentNaming
 
@@ -6,13 +8,38 @@ namespace ProjectGenesis.Patches
 {
     public static class InitialRecipePatches
     {
+        private static Dictionary<int, RecipeExecuteData> _originRecipeExecuteData;
+
+        private static bool _initialized;
+
         [HarmonyPatch(typeof(GameData), nameof(GameData.SetForNewGame))]
         [HarmonyPrefix]
-        public static void SetRecipeForNewGame(GameData __instance)
+        public static void SetRecipeForNewGame()
         {
             if (DSPGame.LoadDemoIndex > 0 || DSPGame.IsMenuDemo) return;
 
             RecipeProto.InitRecipeItems();
+        }
+
+        [HarmonyPatch(typeof(GameSave), nameof(GameSave.LoadCurrentGameInResource))]
+        [HarmonyPrefix]
+        public static void LoadCurrentGameInResource()
+        {
+            if (DSPGame.LoadDemoIndex > 0 || DSPGame.IsMenuDemo)
+            {
+                if (_initialized)
+                    RecipeProto.recipeExecuteData = _originRecipeExecuteData;
+                else
+                    SaveOriginRecipeExecuteData();
+            }
+        }
+
+        private static void SaveOriginRecipeExecuteData()
+        {
+            if (_initialized) return;
+
+            _originRecipeExecuteData = RecipeProto.recipeExecuteData ?? throw new ArgumentNullException();
+            _initialized = true;
         }
     }
 }
