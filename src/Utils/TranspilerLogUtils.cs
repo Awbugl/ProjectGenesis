760dfbb2a9a19e1aa3b14d43b5ad659d641c0a6d
Awbugl <me@awbugl.top>
2026-02-08 17:30:15 +0800

    Update TranspilerLogUtils.cs

--- src/Utils/TranspilerLogUtils.cs
+++ src/Utils/TranspilerLogUtils.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System.Reflection;
+using HarmonyLib;
+using Mono.Cecil;
+using MonoMod.Utils;
 using ProjectGenesis.Compatibility;
 
 namespace ProjectGenesis.Utils
@@ -15,6 +18,17 @@ namespace ProjectGenesis.Utils
         {
             foreach (CodeInstruction codeInstruction in matcher.InstructionEnumeration())
                 InstallationCheckPlugin.logger.LogInfo(codeInstruction.ToString());
+        }
+
+        public static void ToILDasmString(this MethodBase methodInfo)
+        {
+            using (var methodDefinition = new DynamicMethodDefinition(methodInfo))
+            {
+                string invoke = (string)AccessTools
+                   .Method("HarmonyLib.Internal.Util.MethodBodyLogExtensions:ToILDasmString", new[] { typeof(Mono.Cecil.Cil.MethodBody) })
+                   .Invoke(null, new object[] { methodDefinition.Definition.Body, });
+                InstallationCheckPlugin.logger.LogInfo("Generated IL string for (" + methodInfo.FullDescription() + "):\n" + invoke);
+            }
         }
     }
 }
