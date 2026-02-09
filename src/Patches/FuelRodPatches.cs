d1601f237e3f8573f0b8284f89676691973fc98b
Awbugl <me@awbugl.top>
2026-02-09 18:45:23 +0800

    fix EmptyRod issue

--- src/Patches/FuelRodPatches.cs
+++ src/Patches/FuelRodPatches.cs
@@ -25,66 +25,7 @@ namespace ProjectGenesis.Patches
             ProtoID.I满蓄电器, ProtoID.I能量碎片,
         };
 
-        [HarmonyPatch(typeof(Mecha), nameof(Mecha.GenerateEnergy))]
-        [HarmonyTranspiler]
-        public static IEnumerable<CodeInstruction> SetTargetCargoBytes_Transpiler(IEnumerable<CodeInstruction> instructions)
-        {
-            var matcher = new CodeMatcher(instructions);
-
-            matcher.MatchForward(true, new CodeMatch(OpCodes.Ldc_I4_M1), new CodeMatch(OpCodes.Stloc_S));
-
-            matcher.Advance(1).InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0),
-                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(FuelRodPatches), nameof(GenerateEnergy_Patch))));
-
-            return matcher.InstructionEnumeration();
-        }
-
-        public static void GenerateEnergy_Patch(Mecha mecha)
-        {
-            int count = GetEmptyRodCount(mecha.reactorItemId);
-
-            if (count == 0) return;
-
-            mecha.player.TryAddItemToPackage(ProtoID.I空燃料棒, count, mecha.reactorItemInc, true);
-            UIItemup.Up(ProtoID.I空燃料棒, count);
-        }
-
-        private static int GetEmptyRodCount(int itemId)
-        {
-            var count = 0;
-
-            switch (itemId)
-            {
-                case ProtoID.I氢燃料棒:
-                case ProtoID.I煤油燃料棒:
-                case ProtoID.I四氢双环戊二烯燃料棒:
-                case ProtoID.I铀燃料棒:
-                case ProtoID.I钚燃料棒:
-                case ProtoID.I氘核燃料棒:
-                case ProtoID.I氦三燃料棒:
-                case ProtoID.I反物质燃料棒:
-                    count = 1;
-
-                    break;
-
-                case ProtoID.I氘氦混合聚变燃料棒:
-                    count = 2;
-
-                    break;
-
-                case ProtoID.IMOX燃料棒:
-                    count = 3;
-
-                    break;
-
-                case ProtoID.I奇异燃料棒:
-                    count = 4;
-
-                    break;
-            }
-
-            return count;
-        }
+        public static bool IsFuelRod(ItemProto proto) => FuelRods.Contains(proto.ID) || proto.FuelType == 31;
 
         [HarmonyPatch(typeof(UIStorageGrid), nameof(UIStorageGrid.GetOtherStorageGrid), typeof(int))]
         [HarmonyTranspiler]
@@ -120,8 +61,6 @@ namespace ProjectGenesis.Patches
 
             return matcher.InstructionEnumeration();
         }
-
-        public static bool IsFuelRod(ItemProto proto) => FuelRods.Contains(proto.ID) || proto.FuelType == 31;
 
         [HarmonyPatch(typeof(UIStorageGrid), nameof(UIStorageGrid.HandPut))]
         [HarmonyTranspiler]
