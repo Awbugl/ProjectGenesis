255a5c2ae4559cca2a5e0d96fb9fd0c59980af32
Awbugl <me@awbugl.top>
2026-03-08 10:44:38 +0800

    Update TankComponentPatches.cs

--- src/Patches/Hooks/TankComponentPatches.cs
+++ src/Patches/Hooks/TankComponentPatches.cs
@@ -28,7 +28,7 @@ namespace ProjectGenesis.Patches
                 if (cargoTraffic.TryInsertItemAtHead(this.belt0, this.fluidId, (byte) currentOutputStack, (byte) inc2 * currentOutputStack))
                 {
                     this.fluidCount -= currentOutputStack;
-                    this.fluidInc -= inc2;
+                    this.fluidInc -= inc2 * currentOutputStack;
                 }
             */
 
@@ -36,23 +36,41 @@ namespace ProjectGenesis.Patches
             local.SetLocalSymInfo("currentOutputStack");
             var localIndex = local.LocalIndex;
 
-            matcher.Start().InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0),
-                new CodeInstruction(OpCodes.Call,
-                    AccessTools.Method(typeof(TankComponentPatches), nameof(TankComponent_GameTick_Insert_Method))),
-                new CodeInstruction(OpCodes.Stloc_S, localIndex));
-
             while (true)
             {
                 /*
+                    IL_04ae: ldarg.0      // this
+                    IL_04af: ldfld        int32 TankComponent::fluidInc
+                    IL_04b4: brfalse.s    IL_04c5
 
-                    IL_01e4: ldarg.0      // this
-                    IL_01e5: ldfld        int32 TankComponent::fluidInc
-                    IL_01ea: ldarg.0      // this
-                    IL_01eb: ldfld        int32 TankComponent::fluidCount
-                    IL_01f0: div
-                    IL_01f1: br.s         IL_01f4
-                    IL_01f3: ldc.i4.0
-                    IL_01f4: stloc.s      inc3
+                    IL_04b6: ldarg.0      // this
+                    IL_04b7: ldfld        int32 TankComponent::fluidInc
+                    IL_04bc: ldarg.0      // this
+                    IL_04bd: ldfld        int32 TankComponent::fluidCount
+                    IL_04c2: div
+                    IL_04c3: br.s         IL_04c6
+                    IL_04c5: ldc.i4.0
+                    IL_04c6: stloc.s      inc5
+
+                    // [302 7 - 302 94]
+                    IL_04c8: ldloc.2      // cargoTraffic
+                    IL_04c9: ldarg.0      // this
+                    IL_04ca: ldfld        int32 TankComponent::belt3
+                    IL_04cf: ldarg.0      // this
+                    IL_04d0: ldfld        int32 TankComponent::fluidId
+                    IL_04d5: ldc.i4.1
+                    IL_04d6: ldloc.s      inc5
+                    IL_04d8: conv.u1
+                    IL_04d9: callvirt     instance bool CargoTraffic::TryInsertItemAtHead(int32, int32, unsigned int8, unsigned int8)
+                    IL_04de: brfalse      IL_05df
+
+                    // [304 7 - 304 24]
+                    IL_04e3: ldarg.0      // this
+                    IL_04e4: ldarg.0      // this
+                    IL_04e5: ldfld        int32 TankComponent::fluidCount
+                    IL_04ea: ldc.i4.1
+                    IL_04eb: sub
+                    IL_04ec: stfld        int32 TankComponent::fluidCount
                  */
 
                 matcher.MatchForward(false, new CodeMatch(OpCodes.Ldarg_0),
@@ -61,24 +79,25 @@ namespace ProjectGenesis.Patches
 
                 if (matcher.IsInvalid) break;
 
-                // tankComponent.fluidInc *= currentOutputStack;
+                matcher.InsertAndAdvance(new CodeInstruction(OpCodes.Ldarg_0),
+                    new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(TankComponentPatches), nameof(TankComponent_GameTick_Insert_Method))),
+                    new CodeInstruction(OpCodes.Stloc_S, localIndex));
+
+                // tankComponent.fluidInc * currentOutputStack;
                 matcher.InsertAndAdvance(new CodeInstruction(OpCodes.Ldloc_S, localIndex), new CodeInstruction(OpCodes.Mul));
 
-                matcher.Advance(3);
-            }
+                // stack = (byte) currentOutputStack;
+                matcher.MatchForward(true, new CodeMatch(OpCodes.Ldarg_0),
+                    new CodeMatch(OpCodes.Ldfld, AccessTools.Field(typeof(TankComponent), nameof(TankComponent.fluidId))),
+                    new CodeMatch(OpCodes.Ldc_I4_1));
 
-            matcher.Start();
+                matcher.SetAndAdvance(OpCodes.Ldloc_S, localIndex).InsertAndAdvance(new CodeInstruction(OpCodes.Conv_U1));
 
-            // replace all Ldc_I4_1 to currentOutputStack
-            while (true)
-            {
-                matcher.MatchForward(false, new CodeMatch(OpCodes.Ldc_I4_1));
-
-                if (matcher.IsInvalid) break;
+                // this.fluidCount -= currentOutputStack;
+                matcher.MatchForward(false, new CodeMatch(OpCodes.Ldc_I4_1), new CodeMatch(OpCodes.Sub));
 
                 matcher.SetAndAdvance(OpCodes.Ldloc_S, localIndex);
-
-                matcher.Advance(1);
             }
 
             return matcher.InstructionEnumeration();
@@ -87,7 +106,6 @@ namespace ProjectGenesis.Patches
         public static int TankComponent_GameTick_Insert_Method(ref TankComponent component)
         {
             int componentFluidCount = component.fluidCount;
-
             int historyStationPilerLevel = GameMain.history.stationPilerLevel;
 
             return componentFluidCount < historyStationPilerLevel ? componentFluidCount : historyStationPilerLevel;
